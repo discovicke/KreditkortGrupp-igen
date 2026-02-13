a6091fa7e9544513a852e1581680b9a38d6d5ee3
Viktor Johansson <discovicke@gmail.com>
2026-02-13 11:04:38 +0100

    Add SQLite database integration and data insertion for generated names

--- KreditkortGrupp-igen/Program.cs
+++ KreditkortGrupp-igen/Program.cs
@@ -1,4 +1,12 @@
 ﻿using KreditkortGrupp_igen;
+using Microsoft.Data.Sqlite;
+
+using var connection = new SqliteConnection("Data Source=kreditkort.db");
+connection.Open();
+using var command = connection.CreateCommand();
+command.CommandText =
+    @"CREATE TABLE IF NOT EXISTS People (id INTEGER PRIMARY KEY, Name TEXT NOT NULL, Kreditkort INTEGER)";
+command.ExecuteNonQuery();
 
 
 var firstnamePath = "MOCK_DATA_first_name.json";
@@ -6,7 +14,8 @@ var lastnamePath = "MOCK_DATA-last_name.json";
 
 var arrayCreator = new JsonArrayCreator();
 
-string[] menu = [
+string[] menu =
+[
     "1. Generate mock data?",
     "2. List People?",
     "3. List Cards?",
@@ -20,10 +29,11 @@ var nameList = new List<Name>();
 Console.WriteLine("|=== What do you want to do? ===|\n");
 while (run)
 {
-    foreach(var alternativ in menu)
+    foreach (var alternativ in menu)
     {
         Console.WriteLine(alternativ);
     }
+
     Console.Write("\nChoice: ");
     string? val = Console.ReadLine();
     switch (val)
@@ -33,7 +43,14 @@ while (run)
             var numberOfPeople = int.TryParse(Console.ReadLine(), out int result) ? result : 100000;
             Console.WriteLine("Generating data...");
             nameList = arrayCreator.CreateNameList(firstnamePath, lastnamePath, result);
-            
+            foreach (var name in nameList)
+            {
+                command.CommandText = "INSERT INTO People (Name) VALUES (@Name)";
+                command.Parameters.Clear();
+                command.Parameters.AddWithValue("@Name", name.ToString());
+                command.ExecuteNonQuery();
+            }
+
             Console.WriteLine("Data generated. \nPress any key to return to main menu");
             Console.ReadKey();
             break;
@@ -58,11 +75,11 @@ while (run)
             break;
     }
 }
+
 void PrintNames(List<Name> names)
 {
     foreach (var name in names)
     {
         Console.WriteLine(name);
     }
-    
 }

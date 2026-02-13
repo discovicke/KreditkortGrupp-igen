f953daf294c6ea13a65eb9d75d7e16176651b983
Viktor Johansson <discovicke@gmail.com>
2026-02-13 10:35:07 +0100

    Fix random name generator

--- KreditkortGrupp-igen/JsonArrayCreator.cs
+++ KreditkortGrupp-igen/JsonArrayCreator.cs
@@ -7,23 +7,17 @@ public class JsonArrayCreator
 {
     public List<Name> CreateNameList(string firstnamePath, string lastnamePath, int numberOfNames)
     {
-        
         var jsonFirst = File.ReadAllText(firstnamePath);
         var jsonLast = File.ReadAllText(lastnamePath);
         var deserializeFirst = JsonSerializer.Deserialize<List<FirstName>>(jsonFirst);
         var deserializeLast = JsonSerializer.Deserialize<List<LastName>>(jsonLast);
         
-        
-        
         return MockNameGenerator(deserializeFirst, deserializeLast, numberOfNames);
-
     }
 
     public List<Name> MockNameGenerator(List<FirstName> firstNames, List<LastName> lastNames, int numberOfNames)
     {
         var random = new Random();
-        numberOfNames = random.Next(1, numberOfNames);
-        
         
         return Enumerable.Range(0, numberOfNames)
             .Select(_ => new Name

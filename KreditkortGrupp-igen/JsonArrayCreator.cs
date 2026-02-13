0804999dd20b51d15fe8affb898c68bc8d321f8a
Viktor Johansson <discovicke@gmail.com>
2026-02-13 10:15:04 +0100

    Implement JsonArrayCreator for generating random names from mock data

---
+++ KreditkortGrupp-igen/JsonArrayCreator.cs
@@ -0,0 +1,59 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace KreditkortGrupp_igen;
+
+public class JsonArrayCreator
+{
+    public List<Name> CreateNameList(string firstnamePath, string lastnamePath, int numberOfNames)
+    {
+        
+        var jsonFirst = File.ReadAllText(firstnamePath);
+        var jsonLast = File.ReadAllText(lastnamePath);
+        var deserializeFirst = JsonSerializer.Deserialize<List<FirstName>>(jsonFirst);
+        var deserializeLast = JsonSerializer.Deserialize<List<LastName>>(jsonLast);
+        
+        
+        
+        return MockNameGenerator(deserializeFirst, deserializeLast, numberOfNames);
+
+    }
+
+    public List<Name> MockNameGenerator(List<FirstName> firstNames, List<LastName> lastNames, int numberOfNames)
+    {
+        var random = new Random();
+        numberOfNames = random.Next(1, numberOfNames);
+        
+        
+        return Enumerable.Range(0, numberOfNames)
+            .Select(_ => new Name
+            {
+                FirstName = firstNames[random.Next(firstNames.Count)],
+                LastName = lastNames[random.Next(lastNames.Count)]
+            })
+            .ToList();
+    }
+}
+
+public class FirstName
+{
+    [JsonPropertyName("first_name")]
+    public string FirstNameName { get; set; }
+}
+
+public class LastName
+{
+    [JsonPropertyName("last_name")]
+    public string LastNameName { get; set; }
+}
+
+public class Name
+{
+    public FirstName FirstName { get; set; }
+    public LastName LastName { get; set; }
+
+    public override string ToString()
+    {
+        return $"{FirstName.FirstNameName} {LastName.LastNameName}";
+    }
+}
--- KreditkortGrupp-igen/Program.cs
+++ KreditkortGrupp-igen/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using KreditkortGrupp_igen;
 
-Console.WriteLine("Hello, World!");
+var firstnamePath = "MOCK_DATA_first_name.json";
+var lastnamePath = "MOCK_DATA-last_name.json";
+
+var arrayCreator = new JsonArrayCreator();
+var nameList = arrayCreator.CreateNameList(firstnamePath, lastnamePath, 100);
+
+foreach (var name in nameList)
+{
+    Console.WriteLine(name.ToString());
+}

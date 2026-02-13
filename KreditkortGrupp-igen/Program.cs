256b00ae0a96bc6dacc9199a806dd9f87e87e6e3
Viktor Johansson <discovicke@gmail.com>
2026-02-13 14:16:18 +0100

    Refactor database table creation and data insertion logic for improved performance and clarity

--- KreditkortGrupp-igen/Program.cs
+++ KreditkortGrupp-igen/Program.cs
@@ -1,27 +1,32 @@
 ﻿using KreditkortGrupp_igen;
-using Microsoft.Data.SqlClient;
 using Microsoft.Data.Sqlite;
-using System.ComponentModel.Design;
-using System.Data;
 using System.Diagnostics;
+
 using var connection = new SqliteConnection("Data Source=kreditkort.db");
 connection.Open();
-using var command = connection.CreateCommand();
-command.CommandText =
-    @"
+
+// Create tables with fresh local commands to avoid accidentally reusing a command
+using (var createPeople = connection.CreateCommand())
+{
+    createPeople.CommandText = @"
 CREATE TABLE IF NOT EXISTS People (
     id INTEGER PRIMARY KEY, 
     Name TEXT NOT NULL,
     HasCard BOOLEAN DEFAULT FALSE)
     ";
-command.ExecuteNonQuery();
-command.CommandText = @"
+    createPeople.ExecuteNonQuery();
+}
+
+using (var createCards = connection.CreateCommand())
+{
+    createCards.CommandText = @"
 CREATE TABLE IF NOT EXISTS CreditCards 
 (id INTEGER PRIMARY KEY, 
 CardNumber TEXT NOT NULL,
 PersonId INTEGER, 
 FOREIGN KEY (PersonId) REFERENCES People(id))";
-command.ExecuteNonQuery();
+    createCards.ExecuteNonQuery();
+}
 
 
 var rnd = new Random();
@@ -32,13 +37,13 @@ var lastnamePath = "MOCK_DATA-last_name.json";
 var arrayCreator = new JsonArrayCreator();
 
 string[] menu =
-[
+{
     "1. Generate mock data?",
     "2. List People?",
     "3. List Cards?",
     "4. List Transactions",
     "5. Exit"
-];
+};
 
 bool run = true;
 var nameList = new List<Name>();
@@ -57,74 +62,122 @@ while (run)
     {
         case "1":
         {
+            Console.WriteLine("Number of people to generate (default 100 000: ");
+            var numberOfPeople = int.TryParse(Console.ReadLine(), out int result) ? result : 100000;
+            Console.WriteLine("Generating data...");
+            var sw = Stopwatch.StartNew();
+            nameList = arrayCreator.CreateNameList(firstnamePath, lastnamePath, numberOfPeople);
 
-                Console.WriteLine("Number of people to generate (default 100 000: ");
-                var numberOfPeople = int.TryParse(Console.ReadLine(), out int result) ? result : 100000;
-                Console.WriteLine("Generating data...");
-                var sw = Stopwatch.StartNew();
-                nameList = arrayCreator.CreateNameList(firstnamePath, lastnamePath, result);
-
-                using var transaction = connection.BeginTransaction();
-                command.Transaction = transaction;
-                command.CommandText = @"INSERT INTO People (Name) VALUES (@Name)";
-
-                var nameParam = command.CreateParameter();
-                nameParam.ParameterName = "@Name";
-                command.Parameters.Add(nameParam);
-
-            foreach (var name in nameList)
+            // Speed: adjust pragmas for bulk insert
+            using (var p = connection.CreateCommand())
             {
-                nameParam.Value = name.ToString();
-                command.ExecuteNonQuery();
+                p.CommandText = "PRAGMA synchronous = OFF;";
+                p.ExecuteNonQuery();
+                p.CommandText = "PRAGMA journal_mode = MEMORY;";
+                p.ExecuteNonQuery();
             }
-            transaction.Commit();
-            
-            command.Transaction = transaction;
-            
-            command.CommandText = @"INSERT INTO CreditCards (PersonId, CardNumber) VALUES (@PersonId, @CardNumber)";
 
-                sw.Stop();
-                double seconds = sw.Elapsed.TotalSeconds;
-            var dataToAdd = CardToPerson(param);
-
-            foreach (var person in dataToAdd)
+            // Single transaction for all DB writes in this operation
+            using (var transaction = connection.BeginTransaction())
             {
-                var personIdParam = command.CreateParameter();
-                personIdParam.ParameterName = "@PersonId";
-                personIdParam.Value = person.Key;
-                command.Parameters.Add(personIdParam);
-                
-                var cardNumberParam = command.CreateParameter();
-                cardNumberParam.ParameterName = "@CardNumber";
-                cardNumberParam.Value = person.Value;
-                command.Parameters.Add(cardNumberParam);
-                
-                command.ExecuteNonQuery();
+                // Insert People with a prepared statement
+                using (var insertPersonCmd = connection.CreateCommand())
+                {
+                    insertPersonCmd.Transaction = transaction;
+                    insertPersonCmd.CommandText = "INSERT INTO People (Name) VALUES (@Name)";
+                    var nameParam = insertPersonCmd.CreateParameter();
+                    nameParam.ParameterName = "@Name";
+                    insertPersonCmd.Parameters.Add(nameParam);
+                    insertPersonCmd.Prepare();
+
+                    foreach (var name in nameList)
+                    {
+                        nameParam.Value = name.ToString();
+                        insertPersonCmd.ExecuteNonQuery();
+                    }
+                }
+
+                // Select IDs of users without cards
+                var ids = new List<long>();
+                using (var selectCmd = connection.CreateCommand())
+                {
+                    selectCmd.Transaction = transaction;
+                    selectCmd.CommandText = "SELECT id FROM People WHERE HasCard = 0";
+                    using var reader = selectCmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        ids.Add(reader.GetInt64(0));
+                    }
+                }
+
+                // Prepare insert for cards
+                using (var insertCardCmd = connection.CreateCommand())
+                {
+                    insertCardCmd.Transaction = transaction;
+                    insertCardCmd.CommandText = "INSERT INTO CreditCards (PersonId, CardNumber) VALUES (@PersonId, @CardNumber)";
+                    var personIdParam = insertCardCmd.CreateParameter();
+                    personIdParam.ParameterName = "@PersonId";
+                    insertCardCmd.Parameters.Add(personIdParam);
+                    var cardNumberParam = insertCardCmd.CreateParameter();
+                    cardNumberParam.ParameterName = "@CardNumber";
+                    insertCardCmd.Parameters.Add(cardNumberParam);
+                    insertCardCmd.Prepare();
+
+                    // Assign cards per person according to bucket logic
+                    foreach (var userId in ids)
+                    {
+                        var bucket = (int)(userId % 100);
+                        int numberOfCards = 0;
+                        if (bucket < 70)
+                        {
+                            numberOfCards = 1;
+                        }
+                        else if (bucket < 90)
+                        {
+                            numberOfCards = 2;
+                        }
+                        else
+                        {
+                            numberOfCards = rnd.Next(3, 11);
+                        }
+
+                        for (int i = 0; i < numberOfCards; i++)
+                        {
+                            personIdParam.Value = userId;
+                            cardNumberParam.Value = CreditCard.GenerateCreditCardNumber(rnd);
+                            insertCardCmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+
+                // Optionally mark users as having cards
+                using (var updateCmd = connection.CreateCommand())
+                {
+                    updateCmd.Transaction = transaction;
+                    updateCmd.CommandText = "UPDATE People SET HasCard = 1 WHERE HasCard = 0";
+                    updateCmd.ExecuteNonQuery();
+                }
+
+                transaction.Commit();
             }
 
-            transaction.Commit();
-            
-            
-            sw.Stop();
-            double seconds = sw.Elapsed.TotalSeconds;
-
-                Console.WriteLine($"Data generated in {seconds:F1} seconds. \nPress any key to return to main menu");
-                Console.ReadKey();
-                break;
-
-            }
+            var elapsed = sw.Elapsed.TotalSeconds;
+            Console.WriteLine($"Data generated in {elapsed:F1} seconds. \nPress any key to return to main menu");
+            if (!Console.IsInputRedirected) Console.ReadKey();
+            break;
+        }
 
         case "2":
             PrintNames(nameList);
             Console.WriteLine("Data generated. \nPress any key to return to main menu");
-            
-            Console.ReadKey();
+
+            if (!Console.IsInputRedirected) Console.ReadKey();
             break;
 
         case "3":
             string card = CreditCard.GenerateCreditCardNumber(rnd);
             Console.WriteLine($"Generated card: {card}");
-            
+
             break;
 
         case "4":
@@ -144,65 +197,4 @@ void PrintNames(List<Name> names)
     {
         Console.WriteLine(name);
     }
-}
-
-Dictionary<long, string> CardToPerson(string cardNumber)
-{
-    var listOfUsers = command.CommandText = @"
-        SELECT 
-            Users.Id 
-        FROM
-            Users
-        WHERE HasCard = false";
-    command.ExecuteReader();
-    
-    foreach (user in listOfUsers)
-    {
-        var bucket = user % 100;
-        var toBeInserted = new Dictionary<long, string>();
-        
-        if (bucket < 70)
-        {
-            //get number and assign to var cardNumberToAdd
-            toBeInserted.Add(user, cardNumberToAdd);
-        } 
-        else if (user < 90)
-        {   
-            var numberOfCards = 2;
-            
-            foreach (card in numberOfCards)
-            {
-                //get number and assign to var cardNumberToAdd
-                toBeInserted.Add(user, cardNumberToAdd);
-            }
-        }
-        var rand = new Random();
-        var numberOfCards =  rand.Next(3, 11);
-            
-        foreach (card in numberOfCards)
-        {
-            //get number and assign to var cardNumberToAdd
-            toBeInserted.Add(user, cardNumberToAdd);
-        }
-
-        return toBeInserted;
-    }
-    
-    
-    /*
-     * Plocka ut alla användare ur databsen --> lista
-     * Fördela användare till 3 typer
-     * Insert typ 1 = 1 kort
-     * 2 = 2
-     * 3 = 3-10 (random)
-     * IF redan har nummer = inget händer
-     *
-     * För varje person i databasen ska det finnas ett kort
-     * OM person.id STARTS WITH = 1 > 6
-     * THEN CardNumber.PersonId = person.id på ETT KORT
-     * OM person.id START WITH = 7 > 8
-     * THEN CardNumber.PersonId = person.id på TVÅ KORT
-     * OM person.id START WITH = 9
-     * THEN CardNumber.PersonId = person.id på TRE KORT
-     */
 }

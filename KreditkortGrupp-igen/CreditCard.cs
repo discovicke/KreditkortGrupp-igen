2756c0ad7e98a8972c6afdf90478d903fba1d8dc
discotomten <linkaan_lonn@hotmail.com>
2026-02-13 13:49:42 +0100

    kort test

---
+++ KreditkortGrupp-igen/CreditCard.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace KreditkortGrupp_igen
+{
+    public class CreditCard
+    {
+
+
+        public static string CreditCardGen(string baseNumber)
+        {
+            int sum = 0;
+            bool doubleDigit = true;
+
+            for (int i = baseNumber.Length - 1; i >= 0; i--)
+            {
+                int digit = baseNumber[i] - '0';
+                if (doubleDigit)
+                {
+                    digit *= 2;
+                    if (digit > 9)
+                        digit -= 9;
+                }
+                sum += digit;
+                doubleDigit = !doubleDigit;
+            }
+            int checkDigit = (10 - (sum % 10)) % 10;
+            return baseNumber + checkDigit;
+        }
+        public static string GenerateCreditCardNumber(Random rnd)
+        {
+            string baseNumber = "";
+            for (int i = 0; i < 15; i++)
+                baseNumber += rnd.Next(0, 10);
+            return CreditCardGen(baseNumber);
+        }
+    }
+}
--- KreditkortGrupp-igen/Program.cs
+++ KreditkortGrupp-igen/Program.cs
@@ -1,7 +1,9 @@
 ﻿using KreditkortGrupp_igen;
+using Microsoft.Data.SqlClient;
 using Microsoft.Data.Sqlite;
+using System.ComponentModel.Design;
+using System.Data;
 using System.Diagnostics;
-
 using var connection = new SqliteConnection("Data Source=kreditkort.db");
 connection.Open();
 using var command = connection.CreateCommand();
@@ -18,6 +20,9 @@ CREATE TABLE IF NOT EXISTS CreditCards
 (id INTEGER PRIMARY KEY, 
 CardNumber TEXT NOT NULL)";
 command.ExecuteNonQuery();
+
+
+var rnd = new Random();
 
 var firstnamePath = "MOCK_DATA_first_name.json";
 var lastnamePath = "MOCK_DATA-last_name.json";
@@ -50,45 +55,48 @@ while (run)
     {
         case "1":
         {
-            Console.WriteLine("Number of people to generate (default 100 000: ");
-            var numberOfPeople = int.TryParse(Console.ReadLine(), out int result) ? result : 100000;
-            Console.WriteLine("Generating data...");
-            var sw = Stopwatch.StartNew();
-            nameList = arrayCreator.CreateNameList(firstnamePath, lastnamePath, result);
 
-            using var transaction = connection.BeginTransaction();
-            command.Transaction = transaction;
-            command.CommandText = @"INSERT INTO People (Name) VALUES (@Name)";
+                Console.WriteLine("Number of people to generate (default 100 000: ");
+                var numberOfPeople = int.TryParse(Console.ReadLine(), out int result) ? result : 100000;
+                Console.WriteLine("Generating data...");
+                var sw = Stopwatch.StartNew();
+                nameList = arrayCreator.CreateNameList(firstnamePath, lastnamePath, result);
 
-            var nameParam = command.CreateParameter();
-            nameParam.ParameterName = "@Name";
-            command.Parameters.Add(nameParam);
+                using var transaction = connection.BeginTransaction();
+                command.Transaction = transaction;
+                command.CommandText = @"INSERT INTO People (Name) VALUES (@Name)";
 
-            foreach (var name in nameList)
-            {
-                nameParam.Value = name.ToString();
+                var nameParam = command.CreateParameter();
+                nameParam.ParameterName = "@Name";
+                command.Parameters.Add(nameParam);
+
+                foreach (var name in nameList)
+                {
+                    nameParam.Value = name.ToString();
+                    command.ExecuteNonQuery();
+                }
+                transaction.Commit();
+
+                sw.Stop();
+                double seconds = sw.Elapsed.TotalSeconds;
+
+                Console.WriteLine($"Data generated in {seconds:F1} seconds. \nPress any key to return to main menu");
+                Console.ReadKey();
+                break;
+
             }
-            command.ExecuteNonQuery();
-            transaction.Commit();
-            
-            
-
-            sw.Stop();
-            double seconds = sw.Elapsed.TotalSeconds;
-
-            Console.WriteLine($"Data generated in {seconds:F1} seconds. \nPress any key to return to main menu");
-            Console.ReadKey();
-            break;
-        }
 
         case "2":
             PrintNames(nameList);
             Console.WriteLine("Data generated. \nPress any key to return to main menu");
+            
             Console.ReadKey();
             break;
 
         case "3":
-
+            string card = CreditCard.GenerateCreditCardNumber(rnd);
+            Console.WriteLine($"Generated card: {card}");
+            
             break;
 
         case "4":
